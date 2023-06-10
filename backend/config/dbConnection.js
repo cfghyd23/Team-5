07444219a44a0f14734c5db1725dabdca3c8b669
shrutidebnath <shrutidebnath13@gmail.com>
2023-06-10 20:10:12 +0530

    internModel.js

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -1,20 +1,19 @@
 const mongoose = require("mongoose");
+const CONNECTION_STRING = "mongodb+srv://cfgteam5:letswin@cluster10.chrhucb.mongodb.net/CodeForGood?retryWrites=true&w=majority"
 
 const connectDb = async () => {
-    try {
-        const connect = await mongoose.connect(process.env.CONNECTION_STRING, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log(
-            "Database connected: ",
-            connect.connection.host,
-            connect.connection.name
-        );
-    } catch (err) {
-        console.log(err);
-        process.exit(1);
-    }
+    mongoose.connect(CONNECTION_STRING, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+        .then(() => {
+            console.log(
+                "Database connected: "
+            );
+        })
+        .catch((err) => {
+            console.log("error" + err);
+        })
 };
 
 module.exports = connectDb;
---
+++ backend/models/internModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const internSchema = mongoose.Schema(
+    {
+        // user_id: {
+        //     type: mongoose.Schema.Types.ObjectId,
+        //     required: true,
+        //     ref: "intern",
+        // },
+        name: {
+            type: String,
+            required: [true, "Please add the contact name"],
+        },
+        email: {
+            type: String,
+            required: [true, "Please add the contact email address"],
+        },
+        phone: {
+            type: String,
+            required: [true, "Please add the contact phone number"],
+        },
+        DOB: {
+            type: String,
+            required: [true, "Please add the contact phone number"],
+        },
+        cause_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "causes",
+
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model("Intern", internSchema);
